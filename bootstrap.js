24f9db0c75b255613836cf37cf675109b4127808
Mike Hommey <mh@glandium.org>
2012-01-10 11:21:58 +0100

    Import version 0.1.1

--- bootstrap.js
+++ bootstrap.js
@@ -1,69 +1,32 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is about:startup
- *
- * The Initial Developer of the Original Code is
- * Mozilla Foundation
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Mike Hommey <mh@glandium.org>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cm = Components.manager;
 
 Cm.QueryInterface(Ci.nsIComponentRegistrar);
 
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
 
 function AboutStartup() {}
 
 AboutStartup.prototype = {
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutModule]),
-  classDescription: "about:startup",
-  classID: Components.ID("{ef5c36bf-8559-4449-8133-03e30e83c708}"),
-  contractID: "@mozilla.org/network/protocol/about;1?what=startup",
+  classDescription: 'about:startup',
+  classID: Components.ID('{ef5c36bf-8559-4449-8133-03e30e83c708}'),
+  contractID: '@mozilla.org/network/protocol/about;1?what=startup',
 
   newChannel: function(uri)
   {
-    var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-    var html = "data:text/html,<html><body><table>";
-    var startupInfo = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup_MOZILLA_2_0).getStartupInfo();
+    var ioService = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
+    var html = 'data:text/html,<html><body><table>';
+    var startupInfo = Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup_MOZILLA_2_0).getStartupInfo();
     var keys = Object.keys(startupInfo);
     keys.sort(function(a, b) { return startupInfo[a] - startupInfo[b]; });
     for each (var name in keys)
-      if (name != "process")
-        html += "<tr><td>" + name + "</td><td>" + (startupInfo[name] - startupInfo.process) + "</td></tr>";
-    html += "</table></body></html>";
+      if (name != 'process')
+        html += '<tr><td>' + name + '</td><td>' + (startupInfo[name] - startupInfo.process) + '</td></tr>';
+    html += '</table></body></html>';
     var channel = ioService.newChannel(html, null, null);
-    var securityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
+    var securityManager = Cc['@mozilla.org/scriptsecuritymanager;1'].getService(Ci.nsIScriptSecurityManager);
     var principal = securityManager.getCodebasePrincipal(uri);
     channel.originalURI = uri;
     channel.owner = principal;
@@ -78,13 +41,67 @@ AboutStartup.prototype = {
 
 const AboutStartupFactory = XPCOMUtils.generateNSGetFactory([AboutStartup])(AboutStartup.prototype.classID);
 
+var timer = false;
+
+const PR_WRONLY = 0x02;
+const PR_APPEND = 0x10;
+
+function saveData() {
+  var prefs = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService).getBranch('extensions.aboutstartup.');
+  var last;
+  try {
+    last = prefs.getIntPref('last');
+  } catch(e) {
+    last = 0;
+  }
+  var startupInfo = Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup_MOZILLA_2_0).getStartupInfo();
+  var start = Math.floor(startupInfo.process / 1000);
+  if (last >= start)
+    return;
+
+  var keys = Object.keys(startupInfo);
+  var data = {};
+  for each (var name in keys)
+    if (name != 'process')
+      data[name] = startupInfo[name] - startupInfo.process;
+  var appInfo = Cc['@mozilla.org/xre/app-info;1'].getService(Ci.nsIXULAppInfo);
+  data.version = appInfo.version;
+  data.appBuildID = appInfo.appBuildID;
+  var file = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('ProfD', Ci.nsIFile);
+  file.append('startup.log');
+  if (!file.exists())
+      file.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0666);
+  var foStream = Cc['@mozilla.org/network/file-output-stream;1'].createInstance(Ci.nsIFileOutputStream);
+  foStream.init(file, PR_WRONLY | PR_APPEND, 0666, 0);
+  var writer = Cc["@mozilla.org/intl/converter-output-stream;1"].createInstance(Ci.nsIConverterOutputStream);
+  writer.init(foStream, "UTF-8", 0, 0x0000);
+  writer.writeString(JSON.stringify(data) + "\n");
+  writer.close();
+  prefs.setIntPref('last', start);
+}
+
+const APP_STARTUP = 1;
+const ADDON_ENABLE = 3;
+const ADDON_UPGRADE = 7;
+
 function startup(aData, aReason) {
   Cm.registerFactory(AboutStartup.prototype.classID,
                      AboutStartup.prototype.classDescription,
                      AboutStartup.prototype.contractID,
                      AboutStartupFactory);
+  if (aReason == APP_STARTUP) {
+    timer = Cc['@mozilla.org/timer;1'].createInstance(Ci.nsITimer);
+    timer.initWithCallback(saveData, 10000, Ci.nsITimer.TYPE_ONE_SHOT);
+  } else if ((aReason == ADDON_ENABLE) || (aReason == ADDON_UPGRADE)) {
+    try {
+      saveData();
+    } catch(e) {}
+  }
 }
 function shutdown(aData, aReason) {
+  try {
+    saveData();
+  } catch(e) {}
   Cm.unregisterFactory(AboutStartup.prototype.classID, AboutStartupFactory);
 }
 function install(aData, aReason) { }
